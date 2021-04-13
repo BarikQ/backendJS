15b7156414cf9b344af63e606da5cdecff031e3e
Yaroslav <44002588+BarikQ@users.noreply.github.com>
2021-04-13 17:42:05 +0300

    Delete lesson-4_ Working with Streams directory

--- lesson-4_ Working with Streams/stream/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  Ui:             require('./Ui'),
-  AccountManager: require('./AccountManager'),
-  Guardian:       require('./Guardian'),
-  Logger:         require('./Logger'),
-  Decryptor:      require('./Decryptor'),
-};
